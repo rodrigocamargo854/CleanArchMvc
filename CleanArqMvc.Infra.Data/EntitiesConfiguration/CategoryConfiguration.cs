9ab7510fbf4b9711524268f5ce7e0b33e2e729d9
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2022-11-17 07:22:23 -0300

    CategorryConfiguration ProductConfiguration implemented

---
+++ CleanArqMvc.Infra.Data/EntitiesConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CleanArqMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArqMvc.Infra.Data.EntitiesConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure (EntityTypeBuilder<Category> builder) {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+
+            //using populate to include some registers
+            builder.HasData(
+                new Category(1, "MAterial Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios")
+                );
+        }
+    }
+}
