072c53a8d539d40368115438a0f37455fe6db614
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2022-11-17 08:01:32 -0300

    ICategoryREpository implemented

--- CleanArqMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
+++ CleanArqMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CleanArqMvc.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,19 @@ using System.Threading.Tasks;
 
 namespace CleanArqMvc.Infra.Data.EntitiesConfiguration
 {
-    internal class ProductConfiguration
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
     {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(200).IsRequired();
+
+            builder.Property(p => p.Price).HasPrecision(10,2);
+            //relation one to many products => products
+            builder.HasOne(e => e.Category).WithMany(e => e.Products).HasForeignKey(e => e.CategoryId);
+
+
+        }
     }
 }
